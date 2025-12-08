75828ef33e8473fe8ed8f20b4d82a8dcf85b9d4d
Manohar Munna <manoharmabbu@gmail.com>
2025-12-08 22:15:12 +0530

    idi pakka last

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
-import adminReducer from '../features/auth/adminSlice'; // <--- Import
+// CHANGED: Import the new filename
+import adminReducer from '../features/auth/adminReducer'; 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    admin: adminReducer, // <--- Add here
+    admin: adminReducer,
   },
 });
