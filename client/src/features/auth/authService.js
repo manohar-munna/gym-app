b0f9460a5c8ba3ef30524ab8e758e69dc54dad2f
Manohar Munna <manoharmabbu@gmail.com>
2025-12-08 18:37:55 +0530

    try again js  file auth

--- client/src/features/auth/authService.js
+++ client/src/features/auth/authService.js
@@ -1,29 +1,34 @@
 const API_URL = '/api/users/';
 
+// Register user
 const register = async (userData) => {
   const res = await fetch(API_URL, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(userData),
   });
+
   const data = await res.json();
   if (!res.ok) throw new Error(data.message);
   localStorage.setItem('user', JSON.stringify(data));
   return data;
 };
 
+// Login user
 const login = async (userData) => {
   const res = await fetch(API_URL + 'login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(userData),
   });
+
   const data = await res.json();
   if (!res.ok) throw new Error(data.message);
   localStorage.setItem('user', JSON.stringify(data));
   return data;
 };
 
+// Logout user
 const logout = () => localStorage.removeItem('user');
 
 export default { register, login, logout };
